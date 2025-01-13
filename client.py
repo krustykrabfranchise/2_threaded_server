1c2b14535d93d504da8f7d6067bdef43ffeea67b
author <email@email.com>
2025-01-13 09:29:32 +0400

    add solution

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def echo_client(host='127.0.0.1', port=60000):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+        print("Connected to the server.")
+        while True:
+            message = input("Введите сообщение (напишите 'exit' для выхода): ")
+            if message.lower() == 'exit':
+                break
+            client_socket.sendall(message.encode())
+            data = client_socket.recv(1024)
+            print(f"Ответ от сервера: {data.decode()}")
 
-data = sock.recv(1024)
 
-sock.close()
-
-print(data.decode())
+if __name__ == '__main__':
+    echo_client()
--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+def handle_client(client_socket, client_address):
+    print(f"Подключение от клиента: {client_address}")
+    with client_socket:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                print(f"Отключение клиента: {client_address}")
+                break
+            client_socket.sendall(data)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
+def threaded_echo_server(host='127.0.0.1', port=60000):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((host, port))
+        server_socket.listen()
+        print(f"Многопоточный сервер работает на {host}:{port}")
+        while True:
+            client_socket, client_address = server_socket.accept()
+            thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            thread.start()
 
-conn.close()
+
+if __name__ == '__main__':
+    threaded_echo_server()
